38ede22693e70f7002fc419bed541939bed8f807
Rednut Software <rednutsoftware+github@gmail.com>
2019-01-28 02:02:13 +0000

    enable PERMISSIVE_HOLD option

--- keyboards/iris/keymaps/rednut/config.h
+++ keyboards/iris/keymaps/rednut/config.h
@@ -34,3 +34,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define RGBLIGHT_HUE_STEP 8
 #define RGBLIGHT_SAT_STEP 8
 #define RGBLIGHT_VAL_STEP 8
+
+/* for fast typing */
+#define PERMISSIVE_HOLD
+

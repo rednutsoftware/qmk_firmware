031145db8eec04e679fa4d3f32d696e216600501
Rednut Software <rednutsoftware+github@gmail.com>
2019-02-22 10:47:51 +0000

    change TAPPING_TERM value

--- keyboards/iris/keymaps/rednut/config.h
+++ keyboards/iris/keymaps/rednut/config.h
@@ -36,7 +36,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define RGBLIGHT_VAL_STEP 8
 
 /* for fast typing */
-#define TAPPING_TERM 100
+#define TAPPING_TERM 125
 #define PERMISSIVE_HOLD
 #define TAPPING_FORCE_HOLD
 

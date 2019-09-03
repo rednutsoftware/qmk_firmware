b304060132ab5fe02831ecdd75ceec2214bc6357
Rednut Software <rednutsoftware+github@gmail.com>
2019-09-03 09:57:26 +0000

    add "X" at lower layer

--- keyboards/keebio/iris/keymaps/rednut/keymap.c
+++ keyboards/keebio/iris/keymaps/rednut/keymap.c
@@ -80,7 +80,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //+----+----+----+----+----+----+            +----+----+----+----+----+----+
      LCTL, 1  , 2  , 3  , 4  , 5  ,              6  , 7  , 8  , 9  , 0  ,MINS,
   //+----+----+----+----+----+----+----+  +----+----+----+----+----+----+----+
-     LSFT,xxxx,xxxx,xxxx,xxxx,PSCR,HOME,   END ,xxxx,UNDS,COMM,DOT ,QUES,PLUS,
+     LSFT,xxxx, X  ,xxxx,xxxx,PSCR,HOME,   END ,xxxx,UNDS,COMM,DOT ,QUES,PLUS,
   //+----+----+----+----+----+----+----+  +----+----+----+----+----+----+----+
                          LALT,LCTL,LWR ,   UPR ,LSFT,LGUI 
                       //+----+----+----+  +----+----+----+
